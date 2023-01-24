a1de98ab4838e01fbe8e11e5728168f826d657c8
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2023-01-23 23:58:39 -0700

    Messed with styles
    Messed with adding styles to the layout page similar to sveltekit demo app.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+
+	import './styles.css';
+
+	//export let data: LayoutData;
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>(facebook link)</p>
+<p>The purpose of this group is to organize X-Wing Miniatures players in the Phoenix area so we can share tips, ideas, and when and where games are happening locally with one another. We're committed to growing a fun and helpful community of players, while being respectful of other community members. Keep discussions civil and constructive. There will be zero tolerance for any form of bullying or personal attacks - disagreement is fine, disrespect is not. Please do your best to keep the group's tone light-hearted, inclusive, and constructive. Thanks, and welcome!</p>
+
+<p>(discord link)</p>
+
