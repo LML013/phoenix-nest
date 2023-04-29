fde3749582e8a6d6bff9363b18a25f653e633b1f
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2023-04-29 14:23:11 -0700

    About Page, Location Hardcodes, Header, Footer
    Added about page. Removed discord logo svg because it wasn't working in vercel and I'll need to look at that more later.
    
    Did a bunch of hardcoding for location page. Will replace with a database pull later.
    
    Styled Header to be fixed to top and spaced out nav items.
    
    Played with the footer, not quite successful yet.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
 	import Header from './Header.svelte';
-	import Footer from './Footer.svelte'
+	import Footer from './Footer.svelte';
 
 	import './styles.css';
 
@@ -21,10 +21,11 @@
 <style>
 	main {
 		width: 80%;
-  margin-top: 0;
-  margin-bottom: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 600px;
+		margin-top: 0;
+		margin-bottom: 10px;
+		margin-left: auto;
+		margin-right: auto;
+		max-width: 600px;
+		padding-top: 50px;
 	}
 </style>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
-	export let data: PageData;
+	// export let data: PageData;
 </script>
 
-<h1>Events Page</h1>
+<h2>Events</h2>
+<h3>Upcoming Events</h3>
+<h4>Past Events</h4>
