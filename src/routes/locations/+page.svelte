b60f40c3495078ffeee6ee5fb4bbb236d814cf28
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2023-04-29 10:28:13 -0700

    Added Routes
    Switched to SPA approach. Added routes for events and locations

---
+++ src/routes/locations/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h2>Locations</h2>
+<!-- to be turned into components later -->
+<h3>Games U</h3>
+<p>Home base of Phoenix Squadron</p>
+<address>1550 N Stonehenge Dr #102, Gilbert, AZ 85234</address>
+<h3>Gamers Guild</h3>
+<h3>Tabletop Wargamers</h3>
