2840733bceb8bbbbdf14a322cc4b415a6bbe550e
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2023-05-02 11:26:00 -0700

    Event Data attempt
    Moved Event data from page hardcode to page.server.ts file to see what it would be like. Going to pursue this further as all data could probably be moved to a database.

--- src/routes/events/+page.server.ts
+++ src/routes/events/+page.server.ts
@@ -1,5 +1,24 @@
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	return {};
+	let events = [
+		{
+			event_name: 'Phoenix Squadron Championship 2023 Redemption Tournament Aurek',
+			event_type: 'tournament',
+			event_date: '2023-05-06'
+		},
+		{
+			event_name: 'Phoenix Squadron Championship 2023 Redemption Tournament Besh',
+			event_type: 'tournament',
+			event_date: '2023-05-06'
+		},
+		{
+			event_name: 'Phoenix Squadron Championship 2023 Redemption Tournament Cresh',
+			event_type: 'tournament',
+			event_date: '2023-05-06'
+		}
+
+	];
+
+	return {events};
 }) satisfies PageServerLoad;
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+	import { each } from 'svelte/internal';
 	import type { PageData } from './$types';
 
-	// export let data: PageData;
+	export let data: PageData;
 </script>
 
 <h2>Events</h2>
 <h3>Upcoming Events</h3>
 <!-- TODO: Turn these into components (with their own pages?) later -->
-<h4 class="tournament">Phoenix Squadron Championship 2023 Redemption Tournament Aurek</h4>
 
-<h4 class="tournament">Phoenix Squadron Championship 2023 Redemption Tournament Besh</h4>
-<h4 class="tournament">Phoenix Squadron Championship 2023 Redemption Tournament Cresh</h4>
+{#each data.events as event}
+<h4 class={event.event_type}>{event.event_name}</h4>
+<p>Date: {event.event_date}</p>
+{/each}
 <h4 class="tournament">Phoenix Squadron Championship 2023 Invitation Finals</h4>
 
 <h4>Past Events</h4>
